ee52473f51907c5843e4bade135db0c21765bdb3
Hammam <h.elshtewi@hotmail.com>
2022-10-11 15:57:10 +0100

    updated tests for comment count after comments

--- db/data/test-data/articles.sql
+++
@@ -1,8 +0,0 @@
-\c nc_news_test
-
-
-SELECT articles.*, COUNT(comments.article_id) ::INT as comment_count 
-    FROM articles
-    LEFT JOIN comments ON comments.article_id = articles.article_id 
-    WHERE articles.article_id = 1
-    GROUP BY articles.article_id
