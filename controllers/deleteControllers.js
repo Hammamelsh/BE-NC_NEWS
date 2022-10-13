eafdc80eebcc61cde5e0ae6407daba1283cbb579
Hammam <h.elshtewi@hotmail.com>
2022-10-13 16:16:35 +0100

    completed ticket12- delete comment by comment_id

--- controllers/deleteControllers.js
+++ controllers/deleteControllers.js
@@ -2,8 +2,12 @@ const {removeComment} = require('../models/deleteModels')
 
 exports.deleteComment = (req, res, next)=>{
 const id = req.params.comment_id
+console.log(id)
 removeComment(id).then(()=>{
-    res.status(204);
-}).catch(err)
+    res.status(204).send();
+})
+.catch((err)=>{
     next(err)
+})
+ 
 }
