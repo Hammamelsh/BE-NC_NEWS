f6c068c43f7b445947c29fed60ac73991de50a41
Hammam <h.elshtewi@hotmail.com>
2022-10-13 15:10:08 +0100

    ticket 11 queries completed

--- models/postModels.js
+++ models/postModels.js
@@ -3,7 +3,6 @@ const comments = require("../db/data/test-data/comments");
 
 
 exports.addComment = (id,author,body) =>{
-const theAuthor = ["butter_bridge"]
 
 
    if(!author || !body){
@@ -12,9 +11,6 @@ const theAuthor = ["butter_bridge"]
          msg: "bad request not correct format",
      })}
    
-   // // } else if(){
-
-   // }
       return db.query(`INSERT INTO comments (article_id, author,body) VALUES ($1, $2, $3) RETURNING *;`, [id, author, body]).then(({rows})=>{
         if(rows.length === 0){
          return Promise.reject({
