64e8fafcc7dff7db2d74d1d1f8ef6dc6dd6502af
Hammam <h.elshtewi@hotmail.com>
2022-10-14 14:23:31 +0100

    all heroku work

--- controllers/deleteControllers.js
+++ controllers/deleteControllers.js
@@ -2,7 +2,7 @@ const {removeComment} = require('../models/deleteModels')
 
 exports.deleteComment = (req, res, next)=>{
 const id = req.params.comment_id
-console.log(id)
+
 removeComment(id).then(()=>{
     res.status(204).send();
 })
